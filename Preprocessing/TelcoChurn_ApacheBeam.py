a4dd6540887a08868bc933a7cc99f52e0eb9fd64
fernandofsilva <fernandosk8@gmail.com>
2019-02-07 18:19:04 -0200

    Still not running

--- Preprocessing/TelcoChurn_ApacheBeam.py
+++ Preprocessing/TelcoChurn_ApacheBeam.py
@@ -39,3 +39,39 @@ with beam.Pipeline('DirectRunner') as pipeline:
 
 
 #%%
+# Import libraries
+import os
+import csv
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+# Path to load the file
+path = '/home/esssfff/Documents'
+file = 'telco-customer-churn.csv.gz'
+
+class SplitFn(beam.DoFn):
+    def process(self, element):
+        return next(csv.reader(element))
+
+# Create the pipeline
+with beam.Pipeline(options=PipelineOptions()) as p:
+    
+    # Read each line
+    lines = (p | 'Read_File' >> 
+        beam.io.ReadFromText(os.path.join(path, file)))
+
+    binary = (lines
+        | "Read_Lines" >>
+            beam.ParDo(SplitFn())
+        | "Extract_Fields" >> 
+            beam.Map(lambda fields: (fields[0], (fields[1], fields[2])))
+            )
+
+    (binary 
+        | "Map_Tupples" >> 
+            beam.Map(lambda (binary, data): 
+                '{},{}'.format(binary, ','.join(data))) 
+        | "Write_File" >> 
+            beam.io.textio.WriteToText(os.path.join(path,'extracted')))
+
+    p.run()
