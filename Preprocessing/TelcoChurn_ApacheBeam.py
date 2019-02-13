3df61c3780592eaeed25bb85ad71a40608a13c07
fernandofsilva <fernandosk8@gmail.com>
2019-02-13 17:43:27 -0200

    Finished first Transformation

--- Preprocessing/TelcoChurn_ApacheBeam.py
+++ Preprocessing/TelcoChurn_ApacheBeam.py
@@ -3,75 +3,43 @@
 import os
 import csv
 import apache_beam as beam
-
-#%%
-# Path to load the file
-path = '/home/esssfff/Documents'
-file = 'telco-customer-churn.csv.gz'
-
-def bin(value):
-    if value == 'Yes':
-        return 1
-    else:
-        return 0
-
-
-with beam.Pipeline('DirectRunner') as pipeline:
-    
-    binary = (pipeline
-        | "Read_Csv_File" >> 
-            beam.io.ReadFromText(os.path.join(path, file))
-        | "Read_Lines" >>
-            beam.Map(lambda line: next(csv.reader([line])))
-        | "Extract_Fields" >> 
-            beam.Map(lambda fields: (fields[0], (bin(fields[3]), 
-                                                 bin(fields[4]))))
-            )
-
-    (binary 
-        | "Map_Tupples" >> 
-            beam.Map(lambda (binary, data): 
-                '{},{}'.format(binary, ','.join(data))) 
-        | "Write_File" >> 
-            beam.io.textio.WriteToText(os.path.join(path,'extracted')))
-
-    pipeline.run()
-
-
-#%%
-# Import libraries
-import os
-import csv
-import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
+#%%
 # Path to load the file
 path = '/home/esssfff/Documents'
 file = 'telco-customer-churn.csv.gz'
 
-class SplitFn(beam.DoFn):
+class Split(beam.DoFn):
+    """Converts line into dictionary"""
     def process(self, element):
-        return next(csv.reader(element))
+        element = element.encode('utf-8')
+    
+        cols = ['customerID', 'gender', 'SeniorCitizen', 'Partner', 
+                'Dependents', 'tenure', 'PhoneService', 'MultipleLines', 
+                'InternetService', 'OnlineSecurity', 'OnlineBackup', 
+                'DeviceProtection', 'TechSupport', 'StreamingTV', 
+                'StreamingMovies', 'Contract', 'PaperlessBilling', 
+                'PaymentMethod', 'MonthlyCharges', 'TotalCharges', 'Churn']
+    
+        line = element.split(',')
+
+        return [{k:v for k,v in zip(cols, line)}]
 
 # Create the pipeline
 with beam.Pipeline(options=PipelineOptions()) as p:
     
     # Read each line
     lines = (p | 'Read_File' >> 
-        beam.io.ReadFromText(os.path.join(path, file)))
+        beam.io.ReadFromText(os.path.join(path, file),
+                             skip_header_lines=1))
 
     binary = (lines
         | "Read_Lines" >>
-            beam.ParDo(SplitFn())
-        | "Extract_Fields" >> 
-            beam.Map(lambda fields: (fields[0], (fields[1], fields[2])))
-            )
-
-    (binary 
-        | "Map_Tupples" >> 
-            beam.Map(lambda (binary, data): 
-                '{},{}'.format(binary, ','.join(data))) 
+            beam.ParDo(Split())
         | "Write_File" >> 
-            beam.io.textio.WriteToText(os.path.join(path,'extracted')))
+            beam.io.textio.WriteToText(os.path.join(path,'extracted'))    
+        )    
 
     p.run()
+
